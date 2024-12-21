e554e64f4d641171bc60bee3e2f5a6ea287bf35f
sulincix <parduscix@yandex.ru>
2024-12-21 20:55:40 +0000

    main to init

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/ash
+echo -ne "\033c"
+# silent
+exec > /dev/null
+exec 2> /dev/null
+source /etc/profile
+# run eudev
+/sbin/udevd --daemon
+udevadm trigger -c add
+udevadm settle
+# run dbus
+mkdir -p /run/dbus/
+dbus-daemon --system &
+# run Network Manager
+NetworkManager
+# run dropbear
+mkdir -p /dev/pts
+mount -t devpts devpts /dev/pts
+dropbear -R -E 2>/dev/null | :
+# cttyhack
+exec > /dev/tty1
+exec < /dev/tty1
+exec 2> /dev/tty1
+exec /netinstall/main.sh
