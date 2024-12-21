81e4472c7d1e6f6ea42f3cfe622f56740fbff67f
sulincix <parduscix@yandex.ru>
2024-12-21 20:18:34 +0000

    init

--- main.sh
+++ main.sh
@@ -1,2 +1,11 @@
 #!/bin/ash
+echo -ne "\0c"
+source /etc/profile
+mkdir -p /run/dbus/
+# run dbus
+/sbin/udevd --daemon
+udevadm trigger -c add
+udevadm settle
+# run Network Manager
+NetworkManager
 exec /bin/sh
