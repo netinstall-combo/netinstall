c4c4afeab06b27db6a194fac1338d876331bad53
sulincix <parduscix@yandex.ru>
2024-12-22 17:43:57 +0000

    profile variant selection added

--- modules/profile.sh
+++ modules/profile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-select_profile(){
+select_distro(){
     menu=()
     for profile in /netinstall/profiles/*.ini ; do
         menu+=($(basename $profile) "$(ini_parse distro name < $profile)")
@@ -15,4 +15,19 @@ select_profile(){
             break
         fi
     done
+}
+
+select_profile(){
+    menu=()
+    if [ ! -e /netinstall/data/profile ] ; then
+        select_distro
+    fi
+    for sec in $(ini_list_sections < /netinstall/data/profile | grep -v distro) ; do
+         menu+=("$sec" "$(ini_parse $sec description  < /netinstall/data/profile)" "false")
+    done
+    res=$(dialog --title "Profile Menu" \
+        --output-fd 1 \
+        --checklist "Choose variants:" 15 50 4 \
+        "${menu[@]}")
+    echo $res > /netinstall/data/options
 }
