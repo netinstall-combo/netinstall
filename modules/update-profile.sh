00b1d4ac18bb0e8d10b977881fefc04ef222e8ce
sulincix <parduscix@yandex.ru>
2024-12-25 12:36:32 +0000

    install deps during self update

--- modules/update-profile.sh
+++ modules/update-profile.sh
@@ -14,6 +14,7 @@ update_profile(){
 
 update_self(){
     apk upgrade
+    apk add parted lsblk e2fsprogs cfdisk
     wget -O /tmp/netinstall.zip https://github.com/netinstall-combo/netinstall/archive/refs/heads/master.zip
     cd /
     unzip /tmp/netinstall.zip
